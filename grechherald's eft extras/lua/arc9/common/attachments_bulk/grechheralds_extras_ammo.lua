eb73b2ac51bbac04cc771080ac72281e2f1c8785
GrechHerald <137415131+GrechHerald@users.noreply.github.com>
2026-05-06 22:38:38 +0300

    bullet code
    .50 AE rebalance, mk262 mod 1

--- grechherald's eft extras/lua/arc9/common/attachments_bulk/grechheralds_extras_ammo.lua
+++ grechherald's eft extras/lua/arc9/common/attachments_bulk/grechheralds_extras_ammo.lua
@@ -2,6 +2,7 @@ local ATT = {}
 
 local dmgrange = (GetConVar("arc9_eft_mindmgrange"):GetInt() or 1000)/1000
 local mult9 = GetConVar("arc9_eft_mult_pistol"):GetFloat() or 0.5
+local mult556 = GetConVar("arc9_eft_mult_carabine"):GetFloat() or 0.5
 
 -- Contains all custom ammo type attachments
 
@@ -24,7 +25,7 @@ ATT.EFTRoundName = ".50 AE Explosive"
 
 ATT.DamageMax = 120 * mult9
 ATT.DamageMin = 120 * 0.201 * mult9
-ATT.PhysBulletMuzzleVelocity = 400 /0.0254
+ATT.PhysBulletMuzzleVelocity = 420 /0.0254
 -- ATT.TracerNum = 1
 -- ATT.TracerColor = Color(255, 16, 16)
 ATT.SpreadMult = 1.05
@@ -35,9 +36,9 @@ ATT.HeatPerShotMult = 1.40
 ATT.RangeMin = 10
 ATT.RangeMax = 1000 /0.0254 * dmgrange
 
-ATT.Penetration =      36 *2.54/100/0.0254
-ATT.PenetrationDelta = 40/100
-ATT.ArmorPiercing =    40/100
+ATT.Penetration =      11 *2.54/100/0.0254
+ATT.PenetrationDelta = 15/100
+ATT.ArmorPiercing =    15/100
 ATT.RicochetChance =   10/100
 
 ATT.ExplosionDamage = 25
@@ -90,3 +91,77 @@ ATT.DamageType = DMG_BLAST + DMG_AIRBOAT
 ATT.Category = {"eft_ammo_50ae"}
 
 ARC9.LoadAttachment(ATT, "eft_ammo_50ae_sexplosive")
+
+///////////////////////////////////////      eft_ammo_556_mk262
+
+ATT = {}
+
+ATT.PrintName = "5.56x45mm MK 262 Mod 1"
+ATT.CompactName = "MK262"
+ATT.Icon = Material("entities/eft_attachments/ammo/556/sost.png", "mips smooth")
+ATT.Description = [[A 5.56x45mm NATO MK 262 Mod 1 cartridge with a 5 gram open-point bullet with a copper jacket in a brass case. The cartridge was originally made for the Mk12 Special Purpose Rifle, introduced in 2003 and later modified in 2005 for more reliability, it offers a greater accuracy compared to M855 due to its higher powder load and open-point tip since it puts the majority of the weight of the projectile towards its rear, providing greater stability in flight. It however has less penetration compared to M855 and the MK 318 SOST round but makes up for it with unmatched stopping power.
+
+GrechHerald's EFT Extras]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+
+ATT.HasAmmoooooooo = true 
+
+ATT.EFTRoundName = "5.56x45mm MK 262 Mod 1"
+
+ATT.DamageMax = 80 * mult556
+ATT.DamageMin = 60 * mult556
+ATT.PhysBulletMuzzleVelocity = 871 /0.0254
+-- ATT.TracerNum = 1
+-- ATT.TracerColor = Color(255, 16, 16)
+ATT.SpreadMult = 0.9
+-- ATT.RecoilMult = 1.05
+-- ATT.VisualRecoilMult = 1.05
+ATT.HeatPerShotMult = 1
+
+ATT.RangeMin = 10
+ATT.RangeMax = 1000 /0.0254 * dmgrange
+
+ATT.Penetration =      20 *2.54/100/0.0254
+ATT.PenetrationDelta = 20/100
+ATT.ArmorPiercing =    20/100
+ATT.RicochetChance =   10/100
+
+ATT.DamageLookupTable = {
+    {   10/0.0254 * dmgrange, 
+    80 * mult556     },
+
+    {   100 /0.0254 * dmgrange, 
+    79.22 * mult556     },
+
+    {   200 /0.0254 * dmgrange, 
+    77.42 * mult556     },
+
+    {   300 /0.0254 * dmgrange, 
+    75.61 * mult556     },
+
+    {   400 /0.0254 * dmgrange, 
+   73.61 * mult556     },
+
+    {   500 /0.0254 * dmgrange, 
+    71.52 * mult556     },
+
+    {   600 /0.0254 * dmgrange, 
+    69.11 * mult556     },
+
+    {   700 /0.0254 * dmgrange, 
+    66.66 * mult556     },
+
+    {   800 /0.0254 * dmgrange, 
+    64.27 * mult556     },
+
+    {   900 /0.0254 * dmgrange, 
+    62.11 * mult556     },
+
+    {   1000 /0.0254 * dmgrange, 
+    60 * mult556     },
+}
+
+ATT.Category = {"eft_ammo_556"}
+
+ARC9.LoadAttachment(ATT, "eft_ammo_556_mk262")
